cb594eb7085e04de42c937bf242d70bc42e5de53
timetable5678 <coolcats6762@gmail.com>
2024-06-17 02:36:36 +0900

    - 부트스트랩 탬플릿 적용

---
+++ static/js/chart-area-demo.js
@@ -0,0 +1,127 @@
+let myChart;
+let dataTable;
+
+function createChartWithInput() {
+    const param = document.getElementById('paramInput').value;
+    createChart(param);
+}
+
+async function fetchData(param) {
+    const response = await fetch(`/predict?param=${param}`);
+    const json = await response.json();
+    return json;
+}
+
+async function createChart(param) {
+    const data = await fetchData(param);
+
+    const labels = data.data.map(row => new Date(row[0])); // Convert dates to JavaScript Date objects
+    const dataset1 = data.data.map(row => row[1]);
+    const dataset2 = data.data.map(row => row[2]);
+
+    const allData = dataset1.concat(dataset2);
+    const minY = Math.min(...allData);
+
+    const ctx = document.getElementById('myAreaChart').getContext('2d');
+    if (myChart) {
+        myChart.destroy();
+    }
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '1일전 전력',
+                data: dataset1,
+                borderWidth: 1,
+                backgroundColor: '#00C7E2',
+                borderColor : '#00C7E2',
+                fill: false
+            }, {
+                label: '예측 전력',
+                data: dataset2,
+                borderWidth: 1,
+                backgroundColor: '#FF7DA8',
+                borderColor: '#FF7DA8',
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'hour',
+                        //tooltipFormat: 'YYYY-MM-DD HH:mm',
+                        displayFormats: {
+                            hour: 'HH:mm'
+                        }
+                    },
+                    title: {
+                        display: true,
+                        text: '날짜'
+                    }
+                },
+                y: {
+                    suggestedMin: minY,
+                    title: {
+                        display: true,
+                        text: '전력'
+                    }
+                }
+            },
+            plugins: {
+                tooltip: {
+                    enabled: true, // 툴팁을 활성화
+                    titleFont: {size: 25},
+                    bodyFont: {size: 20, color: '#ffffff'},
+                    callbacks: {
+                        label: function (context) {
+                            const label = context.dataset.label || '';
+                            const value = Math.round(context.raw);
+                            return `${label}: ${value}`;
+                        }
+                    }
+                }
+            }
+        }
+    });
+    if ($.fn.DataTable.isDataTable('#dataTable')) {
+        dataTable.clear().destroy();
+    }
+
+    const tableBody = document.querySelector('#dataTable tbody');
+    tableBody.innerHTML = '';
+
+    const dateFormatOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    };
+
+    data.data.forEach(row => {
+        const tr = document.createElement('tr');
+        const dateTd = document.createElement('td');
+        dateTd.textContent = new Date(row[0]).toLocaleString('sv-SE', dateFormatOptions); // using 'sv-SE' to achieve the desired format
+        tr.appendChild(dateTd);
+
+        const dataset1Td = document.createElement('td');
+        dataset1Td.textContent = row[1];
+        tr.appendChild(dataset1Td);
+
+        const dataset2Td = document.createElement('td');
+        dataset2Td.textContent = row[2];
+        tr.appendChild(dataset2Td);
+
+        tableBody.appendChild(tr);
+    });
+    dataTable = $('#dataTable').DataTable({
+        pageLength: 10
+    });
+
+}
+
+createChart(60);
