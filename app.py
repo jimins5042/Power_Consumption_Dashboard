f6923d0988044baa3bb50784baa41ec4943e4a2b
timetable5678 <coolcats6762@gmail.com>
2024-07-09 01:55:54 +0900

    vercel  배포시도

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 # blueprint 설정
-# 스프링은 이런거 안해도 알아서 잡아줬는데...
 app.register_blueprint(dashboard_controller.ds)
 app.register_blueprint(chatbot_controller.cb)
 
